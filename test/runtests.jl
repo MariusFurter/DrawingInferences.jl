f20c326d30adead1b6893dde97fb9638f18d86f5
Marius Furter <marius.furter@gmail.com>
2024-07-30 11:43:45 +0200

    sample test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,5 @@ using DrawingInferences
 using Test
 
 @testset "DrawingInferences.jl" begin
-    # Write your tests here.
+    @test named_categorical([:a, :b, :c], [0.1, 0.2, 0.7]) in [:a, :b, :c]
 end
