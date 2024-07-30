4a1704e6dec4c2da04677b8d038437ea4d04d6be
Marius Furter <marius.furter@gmail.com>
2024-07-30 12:41:21 +0200

    choice_list and choice_map

--- src/distributions.jl
+++ src/distributions.jl
@@ -1,6 +1,12 @@
 ### Additional Distributions ###
 
-@dist function named_categorical(labels, probs)
+@dist function choice_list(labels::Vector, probs::Vector)
+    index = categorical(probs)
+    labels[index]
+end
+
+@dist function choice_map(labels::Vector, f::Function)
+    probs = map(f, labels)
     index = categorical(probs)
     labels[index]
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,6 @@ using DrawingInferences
 using Test
 
 @testset "DrawingInferences.jl" begin
-    @test named_categorical([:a, :b, :c], [0.1, 0.2, 0.7]) in [:a, :b, :c]
+    @test choice_list([:a, :b, :c], [0.1, 0.2, 0.7]) in [:a, :b, :c]
+    @test choice_map([:a, :b, :c], x -> 1 / 3) in [:a, :b, :c]
 end
